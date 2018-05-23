df34415cb09f690a93695d7f9a16b6ae151025fd
Yina Zhu <yinagit@gmail.com>
2018-05-23 21:23:09 +0000

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,43 @@
+require 'pry'
+class PigLatinizer
+
+  def piglatinize(input_str)
+    x = (input_str.split(" ").length == 1) ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
+    puts x
+    x
+  end
+
+  private
+
+  def consonant?(char)
+    !char.match(/[aAeEiIoOuU]/)
+  end
+
+  def vowels?(char)
+    char.match(/[aAeEiIoOuU]/)
+  end
+
+  def piglatinize_word(word)
+      binding.pry
+    # word starts with vowel
+    if !consonant?(word[0])
+      word = word + "w"
+
+    # word starts with 3 consonants
+    elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
+      word = word.slice(3..-1) + word.slice(0,3)
+    # word starts with 2 consonants
+    elsif consonant?(word[0]) && consonant?(word[1])
+      word = word.slice(2..-1) + word.slice(0,2)
+    # word starts with 1 consonant
+    else
+      word = word.slice(1..-1) + word.slice(0)
+    end
+    word << "ay"
+  end
+
+  def piglatinize_sentence(sentence)
+    sentence.split.collect { |word| piglatinize_word(word) }.join(" ")
+  end
+
+end
